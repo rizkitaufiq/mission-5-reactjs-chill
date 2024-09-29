b23e740bad55504add67bbc3a2d71790474929e5
rizkitaufiqgit config --global user.email <rizkitaufiq97@gmail.com>
2024-09-29 22:39:05 +0700

    feat: update content

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
+import Register from "../pages/Register";
 import Beranda from "../pages/Beranda";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+
+      <Route path="/register" element={<Register />} />
 
       <Route path="/beranda" element={<Beranda />} />
     </Routes>
