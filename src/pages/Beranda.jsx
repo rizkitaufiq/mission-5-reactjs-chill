92358ef4d5a9f3264f0c2bd52f15a11eed7a806b
rizkitaufiqgit config --global user.email <rizkitaufiq97@gmail.com>
2024-10-03 17:11:15 +0700

    feat: update content

--- src/pages/Beranda.jsx
+++ src/pages/Beranda.jsx
@@ -1,14 +1,16 @@
 import Navbar from "../components/organisems/beranda/Navbar";
 import Hero from "../components/organisems/beranda/Hero";
 import Watching from "../components/organisems/beranda/Watching";
+import Rating from "../components/organisems/beranda/Rating";
 
 const Beranda = () => {
   return (
     <div>
-      <body className="bg-mainBG h-screen lg:h-full">
+      <body className="bg-mainBG h-full">
         <Navbar />
         <Hero />
         <Watching />
+        <Rating />
       </body>
     </div>
   );
