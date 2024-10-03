a69b6d17a69c448b9d8f189f73cb086a2886f9a8
rizkitaufiqgit config --global user.email <rizkitaufiq97@gmail.com>
2024-10-04 00:59:43 +0700

    feat: add new realese section

--- src/pages/Beranda.jsx
+++ src/pages/Beranda.jsx
@@ -3,6 +3,7 @@ import Hero from "../components/organisems/beranda/Hero";
 import Watching from "../components/organisems/beranda/Watching";
 import Rating from "../components/organisems/beranda/Rating";
 import Trending from "../components/organisems/beranda/Trending";
+import Realese from "../components/organisems/beranda/Realese";
 
 const Beranda = () => {
   return (
@@ -13,6 +14,7 @@ const Beranda = () => {
         <Watching />
         <Rating />
         <Trending />
+        <Realese />
       </div>
     </div>
   );
